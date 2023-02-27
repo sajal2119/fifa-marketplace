4d7e9d977348e1f0344e0d59dba0e7b32a293208
“sajal2119” <sajal2119@gmail.com>
2023-02-28 02:17:40 +0530

    chore: removes icons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,12 +1,4 @@
-export { default as GithubIcon } from "./GithubIcon";
-export { default as LinkedinIcon } from "./LinkedinIcon";
-export { default as NextjsIcon } from "./NextjsIcon";
-export { default as PankodIcon } from "./PankodIcon";
-export { default as TwitterIcon } from "./TwitterIcon";
-export { default as YoutubeIcon } from "./YoutubeIcon";
-export { default as SunIcon } from "./SunIcon";
 export { default as FifaLogo } from "./FifaLogo";
 export { default as TicketIcon } from "./Ticket";
-export { default as MoonIcon } from "./MoonIcon";
 export { default as UserIcon } from "./UserIcon";
 export { default as HamburgerIcon } from "./HamburgerIcon";
